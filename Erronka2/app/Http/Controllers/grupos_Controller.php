cdd414a8d33ff8e174397de4198b80921525dd06
Ander <115074583+EeeeSIGracias@users.noreply.github.com>
2024-01-10 10:35:49 +0100

    cambios

--- Erronka2/app/Http/Controllers/grupos_Controller.php
+++ Erronka2/app/Http/Controllers/grupos_Controller.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\erabiltzailea;
+use App\Models\langilea;
 use App\Models\taldea;
 
 class grupos_Controller extends Controller
 {
     //
     public function erakutsi() {
-        $emaitza=taldea::all();
-        return $emaitza;
+        $emaitza = taldea::select('taldea.izena', langilea::raw('COUNT(langilea.kodea) as langileak'))
+        ->join('langilea', 'taldea.kodea', '=', 'langilea.kodea')
+        ->groupBy('taldea.izena')
+        ->get();
+        // $emaitza=taldea::all();
+        return json_encode($emaitza);
     }
 
 
